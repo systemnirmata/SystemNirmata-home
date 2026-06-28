f466a5430f826c992dfc8f1d151e60579e4a1c4f
systemnirmata <systemnirmata@gmail.com>
2026-06-28 19:36:55 +0530

    sitemap file add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        Sitemap({
+            hostname: 'https://systemnirmata.in/',
+            dynamicRoutes: [
+                '/',
+                '/about',
+                '/contact',
+                '/services',
+                '/work',
+            ],
+        }),
+    ],
 })
